725e79bb24d547112889fa8b6e88a3abcf06d885
sahinmaral <sahin.maral@hotmail.com>
2026-07-26 05:49:06 +0300

    fix: remove PhoneNumberConfirmed field from User entity

--- src/mainPackages/codeTalks.Persistence/Migrations/20260726024814_RemovePhoneNumberConfirmedFromUserEntity.Designer.cs
+++ src/mainPackages/codeTalks.Persistence/Migrations/20260726024814_RemovePhoneNumberConfirmedFromUserEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using codeTalks.Persistence.Contexts;
@@ -11,9 +12,11 @@ using codeTalks.Persistence.Contexts;
 namespace codeTalks.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260726024814_RemovePhoneNumberConfirmedFromUserEntity")]
+    partial class RemovePhoneNumberConfirmedFromUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
